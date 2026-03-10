ed4a4b57ed8c710a95692b2cf7117a35423d238d
Benjamin Pereira Herrera <benjaminpereina@gmail.com>
2026-03-10 18:03:31 -0300

    Backup inicial proyecto WordPress

---
+++ wp-content/themes/hestia-hijo/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Footer personalizado para el tema hijo de Hestia
+ *
+ * @package Hestia hijo
+ */
+?>
+<footer id="colophon" class="site-footer" style="background-color: #333; color: #fff; text-align: center; padding: 20px;">
+    <div class="footer-credits">
+        <p>© 2024 Benjamin Pereira Herrera. Todos los derechos reservados.</p>
+        <div style="margin: 10px 0;">
+            <a href="https://wa.me/3815849276" target="_blank" style="margin-right: 15px; text-decoration: none; color: inherit;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" style="width: 30px; vertical-align: middle;">
+            </a>
+            <a href="mailto:benjaminpereina@gmail.com" style="text-decoration: none; color: inherit;">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" alt="Gmail" style="width: 30px; vertical-align: middle;">
+            </a>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
